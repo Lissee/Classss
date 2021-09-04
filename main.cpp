e294387a5b761a50f4bc18f81803c256595cc1df
Lissee <72330188+Lissee@users.noreply.github.com>
2021-09-05 04:10:04 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,27 @@
 using namespace std;
 
 int main() {
-//1-ый этап + 2 этап
+//1-ый этап + 2 этап + 3 этап
     Students S;
     Professor P("Alexander", "Vladimirov");
     S.SetStudents({"Anton", "Timur", "Lena"}, {"Shevchenko", "Borisov", "Avdaseva"});
     cout<<endl;
-    P.SetGrades("Timur", "Borisov", {5, 5, 5}, S);
-    S.GetStudent("Timur", "Borisov");
-    P.SetGrades("Lena", "Avdaseva", {5, 5, 5, 3, 5}, S);
-    S.GetStudent("Lena", "Avdaseva");
-
+    for (int x = 0; x < 3; x++) {
+        P.SetGrades("Timur", "Borisov", S);
+        S.GetStudent("Timur", "Borisov");
+        P.SetGrades("Lena", "Avdaseva", S);
+        S.GetStudent("Lena", "Avdaseva");
+        S.GetGrades("Timur", "Borisov");
+        S.GetGrades("Lena", "Avdaseva");
+    }
+    P.SetMood("Bad");
+    for (int x = 0; x < 3; x++) {
+        P.SetGrades("Timur", "Borisov", S);
+        S.GetStudent("Timur", "Borisov");
+        P.SetGrades("Lena", "Avdaseva", S);
+        S.GetStudent("Lena", "Avdaseva");
+        S.GetGrades("Timur", "Borisov");
+        S.GetGrades("Lena", "Avdaseva");
+    }
     return 0;
 }
