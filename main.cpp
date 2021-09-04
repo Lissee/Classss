c2c9bfe3f379166fa4d018150f79eee1a43cbf08
Lissee <72330188+Lissee@users.noreply.github.com>
2021-09-05 04:40:39 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int main() {
-//1-ый этап + 2 этап + 3 этап
+//1-ый этап + 2 этап + 3 этап + 4 этап
     Students S;
     Professor P("Alexander", "Vladimirov");
-    S.SetStudents({"Anton", "Timur", "Lena"}, {"Shevchenko", "Borisov", "Avdaseva"});
+    S.SetStudents({"Anton", "Timur", "Lena", "Anastasia", "Vadim", "Ira", "Vika", "Egor"},
+                  {"Shevchenko", "Borisov", "Avdaseva","Lisitcyna", "Dyagilev", "Safonova", "Safonova", "Dyagilev"});
     cout<<endl;
     for (int x = 0; x < 3; x++) {
         P.SetGrades("Timur", "Borisov", S);
@@ -26,5 +27,7 @@ int main() {
         S.GetGrades("Timur", "Borisov");
         S.GetGrades("Lena", "Avdaseva");
     }
+    Para R;
+    R.Paraa(P,S);
     return 0;
 }
