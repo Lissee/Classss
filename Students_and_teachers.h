3a6c20dde85bcfa7fd5b493f55397136e46cc74f
Lissee <72330188+Lissee@users.noreply.github.com>
2021-09-05 02:36:07 +1000

    Update Students_and_teachers.h

--- Students_and_teachers.h
+++ Students_and_teachers.h
@@ -7,6 +7,7 @@ protected:
    string name;
    string surname;
   vector<int> grades;
+friend class Professor;
 
 public:
     Student() = default;
@@ -40,9 +41,10 @@ public:
     }
 };
 
-class Students : Student{
+class Students : public Student{
 protected:
     vector<Student> names_1;
+    friend class Professor;
 public:
     Students() = default;
     void SetStudents(vector<string> names, vector<string> surnames){
@@ -56,22 +58,43 @@ public:
         }
     }
 
-    void GetStudent (int x) {
-        cout << names_1[x].GetName() << " "<< names_1[x].GetSurname()<<endl;
-    }
-    void GetStudent () {
+    void GetStudent (string name, string surname) {
+        bool check = true;
         for (int x = 0; x < names_1.size(); x++) {
-            cout << names_1[x].GetName() << " "<< names_1[x].GetSurname()<<endl;
+            if (name == names_1[x].GetName() && surname == names_1[x].GetSurname()) {
+                cout << names_1[x].GetName() << " "<< names_1[x].GetSurname() << " - " << names_1[x].Five()<<endl;
+                check = false;
+            }
         }
+        if (check) {cout << "This student is not in the database"<<endl;}
     }
 
-    void SetGrades (int x, vector<int> y) {
-        names_1[x].SetGrade(y);
-    }
-    void GetGrades (int x) {
-       cout <<  names_1[x].Five() <<endl;
+    void GetStudent () {
+        for (int x = 0; x < names_1.size(); x++) {
+            cout << names_1[x].GetName() << " "<< names_1[x].GetSurname()<< " - " << names_1[x].Five()<<endl;
+        }
     }
 };
+
+class Professor {
+protected:
+    string name;
+    string surname;
+public:
+    Professor() = default;
+    Professor ( string name, string surname) : name(name), surname(surname){}
+
+    void SetGrades (string names, string surnames, vector<int> y, Students &A) {
+        bool check = true;
+        for (int x = 0; x < A.names_1.size(); x++){
+            if (names == A.names_1[x].GetName() && surnames == A.names_1[x].GetSurname()){
+        A.names_1[x].SetGrade(y);
+            check = false;}
+        }
+        if (check) { cout << "This student is not in the database"<<endl;}
+    }
+};
+
 
 #ifndef UNTITLED19_STUDENTS_AND_TEACHERS_H
 #define UNTITLED19_STUDENTS_AND_TEACHERS_H
